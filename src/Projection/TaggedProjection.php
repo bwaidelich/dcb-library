2a2853114a01c45e3b3070d2559b4091ae790d9f
bwaidelich <b.waidelich@wwwision.de>
2023-09-05 15:11:30 +0200

    TASK: Rename `InMemoryProjection` to `TaggedProjection`
    and make it a decorator for any other `Projection`

--- src/Projection/TaggedProjection.php
+++ src/Projection/TaggedProjection.php
@@ -4,45 +4,44 @@ declare(strict_types=1);
 
 namespace Wwwision\DCBLibrary\Projection;
 
-use Closure;
 use Wwwision\DCBEventStore\Types\EventEnvelope;
 use Wwwision\DCBEventStore\Types\EventTypes;
 use Wwwision\DCBEventStore\Types\StreamQuery\Criteria\EventTypesAndTagsCriterion;
+use Wwwision\DCBEventStore\Types\StreamQuery\Criteria\TagsCriterion;
 use Wwwision\DCBEventStore\Types\StreamQuery\StreamQuery;
 use Wwwision\DCBEventStore\Types\Tag;
 use Wwwision\DCBEventStore\Types\Tags;
 use Wwwision\DCBLibrary\DomainEvent;
+use Wwwision\DCBLibrary\EventTypesAware;
 use Wwwision\DCBLibrary\StreamQueryAware;
+use Wwwision\DCBLibrary\TagsAware;
 
 /**
  * @template S
  * @implements Projection<S>
  */
-final class InMemoryProjection implements Projection, StreamQueryAware
+final class TaggedProjection implements Projection, StreamQueryAware, EventTypesAware, TagsAware
 {
     /**
-     * @param array<class-string<DomainEvent>, Closure(S, DomainEvent, EventEnvelope): S> $handlers
-     * @param S $initialState
+     * @param ClosureProjection<S> $wrapped
      */
     private function __construct(
         private readonly Tags $tags,
-        private readonly array $handlers,
-        private readonly mixed $initialState = null,
+        private readonly Projection $wrapped,
     ) {
     }
 
     /**
+     * @template PS
      * @param Tags|Tag $tags
-     * @param array<class-string<DomainEvent>, Closure> $handlers
-     * @param S $initialState
-     * @return self<S>
+     * @param Projection<PS> $handlers
+     * @return self<PS>
      */
-    public static function create(Tags|Tag $tags, array $handlers, mixed $initialState): self
+    public static function create(Tags|Tag $tags, Projection $handlers): self
     {
         return new self(
             $tags instanceof Tag ? Tags::create($tags) : $tags,
             $handlers,
-            $initialState,
         );
     }
 
@@ -51,7 +50,7 @@ final class InMemoryProjection implements Projection, StreamQueryAware
      */
     public function initialState(): mixed
     {
-        return $this->initialState;
+        return $this->wrapped->initialState();
     }
 
     /**
@@ -60,18 +59,27 @@ final class InMemoryProjection implements Projection, StreamQueryAware
      */
     public function apply(mixed $state, DomainEvent $domainEvent, EventEnvelope $eventEnvelope): mixed
     {
-        if (!array_key_exists($domainEvent::class, $this->handlers)) {
-            return $state;
-        }
         if (!$domainEvent->tags()->containEvery($this->tags)) {
             return $state;
         }
-        return $this->handlers[$domainEvent::class]($state, $domainEvent, $eventEnvelope);
+        return $this->wrapped->apply($state, $domainEvent, $eventEnvelope);
     }
 
     public function adjustStreamQuery(StreamQuery $query): StreamQuery
     {
-        $eventTypes = EventTypes::fromStrings(...array_map(static fn($domainEventClassName) => substr($domainEventClassName, strrpos($domainEventClassName, '\\') + 1), array_keys($this->handlers)));
-        return $query->withCriterion(new EventTypesAndTagsCriterion($eventTypes, $this->tags));
+        if ($this->wrapped instanceof EventTypesAware) {
+            return $query->withCriterion(new EventTypesAndTagsCriterion($this->wrapped->eventTypes(), $this->tags));
+        }
+        return $query->withCriterion(new TagsCriterion($this->tags));
+    }
+
+    public function eventTypes(): EventTypes
+    {
+        return $this->wrapped->eventTypes();
+    }
+
+    public function tags(): Tags
+    {
+        return $this->tags;
     }
 }
